07c1c794695558984271622e33ae478df5d29b89
N-takumi <takumi0106n@gmail.com>
2016-11-27 00:25:08 +0900

    todolist

--- box/Todo/src/todo.js
+++ box/Todo/src/todo.js
@@ -1,5 +1,44 @@
 function todo(){
 
+  varsion = 0.1;
+
+
+  function init(){
+
+    load();//各種ロード
+
+    //[+ボタン]でTodoを追加する
+    $("#addbtn").on('click', function(){
+      todo = window.prompt("Todoを追加します","");
+      if(todo != null){
+      localStorage.setItem("todo"+(localStorage.length+1),todo);
+      $("#list").append($("<h1>").text("・"+localStorage.getItem("todo"+(localStorage.length))));
+      }
+    });
+
+    //[リセットボタン]でTodo全削除
+    $("#resbtn").on('click', function(){
+
+      localStorage.clear();
+      $("#list h1").remove();
+
+    });
+
+
+
+  }
+
+  //localStorageにtodoがあれば表示/バージョンの更新
+  function load(){
+    if(localStorage.length > 0){
+      for(i = 1;i <= localStorage.length;i++){
+      $("#list").append($("<h1>").text("・"+localStorage.getItem("todo"+i)));
+      }
+    }
+  }
+
+
+  init();
 }
 
 
